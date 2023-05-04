d201f909cd629f102237b3aba7c57947c7dbb3e6
guicoelhodev <gs.coelho_dev@outlook.com>
2023-05-03 21:54:30 -0300

    feat: add vitest library and add first test

---
+++ src/app/__tests__/App.spec.tsx
@@ -0,0 +1,7 @@
+import { describe, test, expect } from "vitest";
+
+describe("should nothing", () => {
+  test("djshjkdh", () => {
+    expect(5).toBe(5);
+  });
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { App } from "./app/index";
 import "./style/main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
