639a6022cc2e88bebe7284fd3ce611e2e3291fb7
guicoelhodev <gs.coelho_dev@outlook.com>
2023-05-04 23:41:31 -0300

    feat: add Username component to test into Playground´s page

--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -1,14 +1,15 @@
-import { FC } from 'react'
-import { SendMessage } from 'src/components/SendMessage'
+import { FC } from "react";
+import { Username } from "src/components/Username";
 
 export const Playground: FC = () => {
   return (
-    <div aria-label='playground_page' className='min-h-screen bg-indigo-950 flex flex-col items-center justify-evenly'>
-      <h1 className='text-center text-3xl text-blue-500'>Playground's page</h1>
-
-      <section className='w-full max-w-2xl'>
-        <SendMessage onClickFn={(message) => console.log(message)} />
+    <div
+      aria-label="playground_page"
+      className="min-h-screen bg-primary-blue flex flex-col items-center justify-evenly"
+    >
+      <section className="w-full max-w-2xl">
+        <Username isEditable />
       </section>
     </div>
-  )
-}
+  );
+};
