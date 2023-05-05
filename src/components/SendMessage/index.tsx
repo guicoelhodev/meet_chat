2dd7cc570dc6132a493c301f8cadcd94300bea76
guicoelhodev <gs.coelho_dev@outlook.com>
2023-05-04 23:40:29 -0300

    feat: change css colors and add blur function

--- src/components/SendMessage/index.tsx
+++ src/components/SendMessage/index.tsx
@@ -1,36 +1,47 @@
-import { FC, KeyboardEvent, useState } from 'react'
-import { Icon } from '@iconify/react'
+import { FC, KeyboardEvent, useRef, useState } from "react";
+import { Icon } from "@iconify/react";
 
 interface ISendMessage {
   onClickFn: (message: string) => void;
 }
 
 export const SendMessage: FC<ISendMessage> = ({ onClickFn }) => {
-
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const handleClickEvent = () => {
-    onClickFn(message)
-    return setMessage('')
-  }
+    onClickFn(message);
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') return handleClickEvent()
+    inputRef.current?.blur();
+    return setMessage("");
   };
 
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") return handleClickEvent();
+  };
 
   return (
-    <section className='w-full bg-indigo-900 border-solid border-2 rounded-full border-indigo-900 flex gap-4 items-center focus-within:border-slate-400'>
-      <input className='w-full bg-inherit text-slate-50 text-lg rounded-full p-4 pl-6 focus:outline-none'
-        type='text'
+    <section className="w-full bg-secondary-blue border-solid border border-primary-gray rounded-full flex gap-4 items-center focus-within:border-primary-cyan">
+      <input
+        className="w-full bg-secondary-blue text-secondary-gray text-lg rounded-full p-4 pl-6 focus:outline-none"
+        type="text"
         onChange={(e) => setMessage(e.target.value)}
         onKeyDown={handleKeyDown}
         value={message}
+        ref={inputRef}
       />
 
-      <button className='w-14 bg-inherit aspect-square bg-slate-300 rounded-full grid place-items-center' onClick={() => { onClickFn(message) }}>
-        <Icon icon='ic:baseline-send' className='w-3/5 h-3/5 text-slate-50' />
+      <button
+        className="w-14 bg-inherit aspect-square bg-slate-300 rounded-full grid place-items-center focus:outline-none"
+        onClick={() => {
+          onClickFn(message);
+        }}
+      >
+        <Icon
+          icon="ic:baseline-send"
+          className="w-3/5 h-3/5 text-primary-gray"
+        />
       </button>
     </section>
-  )
-}
+  );
+};
