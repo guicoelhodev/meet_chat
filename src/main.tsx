77b5ce2bc2a64e11a66f1b848160c6259988ae42
guicoelhodev <gs.coelho_dev@outlook.com>
2023-05-07 23:08:57 -0300

    feat: create socket.io client connection

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import { RouterProvider } from "react-router-dom";
-import { router } from './routes'
+import { router } from "./routes";
+import { io } from "socket.io-client";
 
 import "./style/main.css";
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("Connected with socket IO server");
+});
+
+socket.on("disconnect", () => {
+  console.log("Disconnected with socket IO server");
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
