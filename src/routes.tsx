14887289b1366e9c27e8e8f44455bb77234bd974
guicoelhodev <gs.coelho_dev@outlook.com>
2023-05-07 23:08:35 -0300

    feat: create chatRoom page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Playground } from "src/pages/Playground";
+import { ChatRoom } from "./pages/ChatRoom";
 
 export const router = createBrowserRouter([
   {
-    path: '/playground',
-    element: <Playground />
-  }
+    path: "/playground",
+    element: <Playground />,
+  },
+  {
+    path: "/chatRoom",
+    element: <ChatRoom />,
+  },
 ]);
