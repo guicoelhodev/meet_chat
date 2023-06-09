79a206d134701bbdcc71bc38cb74f32126fc8b8d
guicoelhodev <gs.coelho@outlook.com>
2023-06-08 23:14:57 -0300

    feat: add webRTC class into Channel component

--- src/components/Channel/ExpandedChannel/index.tsx
+++ src/components/Channel/ExpandedChannel/index.tsx
@@ -1,8 +1,11 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useRef, useState } from "react";
 
 import Mic from "src/assets/channel_icon/mic.svg";
 import Webcam from "src/assets/channel_icon/webcam.svg";
 import CloseCall from "src/assets/channel_icon/close_call.svg";
+import { WebRTCConnection } from "src/services/webrtc";
+
+
 interface IExpandedChannel {
   isOpen: boolean;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -37,49 +40,78 @@ const mockUsers = [
   // Add more objects as needed
 ];
 
-type IControls = { action: string; icon: any };
+type IControls = { action: string; icon: any; onClick: () => void };
 
 export const ExpandedChannel: React.FC<IExpandedChannel> = ({
   isOpen,
   setIsOpen,
 }) => {
+  const webRTC = new WebRTCConnection();
+  const [userVideo, setUserVideo] = useState(false);
+
+  //const { id: userId } = userStore();
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const getUserCamera = async() => {
+    // console.log('bati')
+     setUserVideo(prev => !prev);
+
+    await webRTC.getUserCamera(videoRef);
+
+  };
+
   const controls: IControls[] = [
     {
       action: "get out call",
       icon: CloseCall,
+      onClick: () => {},
     },
     {
       action: "open camera",
       icon: Webcam,
+      onClick: () => getUserCamera(),
     },
     {
       action: "mute mic",
       icon: Mic,
+      onClick: () => {},
     },
     {
       action: "get out call",
       icon: CloseCall,
+      onClick: () => {},
     },
   ];
+
   return (
     <>
       {isOpen ? (
         <div
           className="z-20 w-screen h-screen bg-primary-blue bg-opacity-20 backdrop-blur-md fixed grid place-items-center"
-          onClick={() => setIsOpen(false)}
         >
-          <section className="bg-secondary-blue w-full max-w-4xl rounded-sm p-8 flex flex-col items-center gap-4 border-red-100 border relative">
+          <section className="bg-secondary-blue w-full max-w-4xl rounded-sm p-8 flex flex-col items-center gap-4 border-red-400 border relative">
             <ul className="w-full grid grid-cols-2 grid-rows-2 gap-4">
-              {mockUsers.map((user) => (
+              {mockUsers.map((user, index) => (
                 <li
                   key={user.id}
-                  className="bg-blue-800 flex flex-col items-center gap-4 p-4 rounded-md text-xl"
+                  className=" h-56 bg-blue-800 flex flex-col items-center justify-center gap-4 rounded-md text-xl overflow-hidden"
                 >
-                  <img
-                    src={user.photo}
-                    className="w-28 aspect-square rounded-full"
-                  />
-                  <p>{user.name}</p>
+                  {userVideo ? (
+                      <video
+                      ref={index === 0 ? videoRef : null}
+                       className="w-full mb-4"
+                       autoPlay
+                      ></video>
+                  ) : (
+                    <>
+                      <img
+                        src={user.photo}
+                        className="w-28 aspect-square rounded-full"
+                      />
+                      <p>{user.name}</p>
+                    </>
+                  )}
                 </li>
               ))}
             </ul>
@@ -89,15 +121,19 @@ export const ExpandedChannel: React.FC<IExpandedChannel> = ({
                 <button
                   className="bg-blue-900 p-4 rounded-full"
                   title={control.action}
+                  onClick={control.onClick}
                 >
                   <img className="w-8 h-8" src={control.icon} />
                 </button>
               ))}
             </article>
-            {/* 
-            <button className="absolute top-4 right-4 bg-blue-900 p-4 rounded-full border-slate-100 border-2">
+
+            <button
+              onClick={() => setIsOpen(false)}
+              className="absolute top-4 right-4 bg-blue-900 p-4 rounded-full border-slate-100 border-2"
+            >
               <img src={CloseCall} className="w-6 h-6" />
-            </button> */}
+            </button>
 
             <aside className="absolute bg-blue-900 bottom-4 right-8 h-20 aspect-video rounded-md grid place-items-center">
               {/* <div className="bg-primary-blue p-8 w-6 h-6 flex items-center justify-center rounded-full"> */}
